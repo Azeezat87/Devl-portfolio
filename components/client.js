7a4006c4bc3e8820078d010740c3c197f2c09a2d
dev-azeezat <azeezatadebola87@gmail.com>
2023-01-25 10:59:18 +0100

    new updates

--- components/client.js
+++ components/client.js
@@ -3,23 +3,7 @@ import { useState } from "react";
 const id = () => Math.random().toString(36);
 
 export default function Client() {
-  const [isHovering, setIsHovering] = useState(new Array(6).fill(false))
-
-  function handleMouseOver(i){
-     setIsHovering((prev) => {
-      const next = [...prev]
-      next[i] = true
-      return next
-     });
-  };
-
-  const handleMouseOut = (i) => {
-    setIsHovering((prev) => {
-      const next = [prev]
-      next[i] = false
-      retrun
-    });
-  };
+  // const [isHovering, setIsHovering] = useState(false)
 
   
 
@@ -38,23 +22,13 @@ export default function Client() {
         <div
           id={id()}
           key={index}
-          onMouseOver={() => handleMouseOver(item)}
-          onMouseOut={handleMouseOut}
+          // onMouseOver={() => setIsHovering(true)}
+          // onMouseOut={() => setIsHovering(false)}
           className='bg-[#f1f1f1] p-16 flex justify-center hover:bg-white transition duration-1000 ease-in-out'
         >
           {item}
-          {isHovering && (
-            <div>
-          <p className='text-xs'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni,
-            nisi.{' '}
-          </p>
-           </div>
-      )}
         </div>
       ))}
-
-      
     </section>
   );
 }
